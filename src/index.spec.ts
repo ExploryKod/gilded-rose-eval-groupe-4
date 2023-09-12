f794c700e6e505a2fe5f6ce6d0c63cb3fad29d19
Amaury Franssen <amauryfranssen@gmail.com>
2023-09-12 08:27:48 +0200

    feat: make a updateQuality class with switch case and only private methods to be call

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,94 @@
+// import runGoldenMaster from "jest-golden-master";
+// import {GildedRose, Item } from "./index";
+
+// test("1 test with Sulfuras item", async () => {
+//   runGoldenMaster(async () => {
+//     const item1 = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+//     const items = new GildedRose([item1]);
+//     items.updateQuality();
+//   });
+// });
+
+// test("2 test with Aged Brie updated 1x with 10 for sellIn and quality", async () => {
+//   runGoldenMaster(async () => {
+//     const item1 = new Item("Aged Brie", 10, 10 );
+//     const items = new GildedRose([item1]);
+//     items.updateQuality();
+//   });
+// });
+
+// test("3 test with Aged Brie when sellIn is negative", async () => {
+//   runGoldenMaster(async () => {
+//     const item1 = new Item("Aged Brie", 2, 10 );
+//     const items = new GildedRose([item1]);
+//     items.updateQuality();
+//     items.updateQuality();
+//     items.updateQuality();
+//   });
+// });
+
+// test("4 test with Backstage passes to a TAFKAL80ETC concert when", async () => {
+//   runGoldenMaster(async () => {
+//     const item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10 );
+//     const items = new GildedRose([item1]);
+//     items.updateQuality();
+//     items.updateQuality();
+//   });
+// });
+
+// test("5 test with Backstage passes to a TAFKAL80ETC concert when sellIn is negative", async () => {
+//   runGoldenMaster(async () => {
+//     const item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 2, 10 );
+//     const items = new GildedRose([item1]);
+//     items.updateQuality();
+//     items.updateQuality();
+//     items.updateQuality();
+//   });
+// });
+
+// test("6 test with several items when quality goes over 50", async () => {
+//   runGoldenMaster(async () => {
+//     const item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 48 );
+//     const item2 = new Item("Aged Brie", 8, 48 );
+//     const items = new GildedRose([item1, item2]);
+//     items.updateQuality();
+//     items.updateQuality();
+//     items.updateQuality();
+//     items.updateQuality();
+//   });
+// });
+
+
+
+
+
+
+
+
+
+// test("7 test with conjured", async () => {
+//   runGoldenMaster(async () => {
+//     const item1 = new Item("Conjured", 15, 3 );
+//     const items = new GildedRose([item1]);
+//     items.updateQuality();
+//     items.updateQuality();
+//     items.updateQuality();
+//     items.updateQuality();
+//   });
+// });
+
+// test("7 test with conjured", async () => {
+//   runGoldenMaster(async () => {
+//     const item1 = new Item("Conjured", 3, 45 );
+//     const items = new GildedRose([item1]);
+//     items.updateQuality();
+//     items.updateQuality();
+//     items.updateQuality();
+//     items.updateQuality();
+//   });
+// });
 import runGoldenMaster from "jest-golden-master";
-import {GildedRose, Item } from "./index";
+import { GildedRose, Item } from "./index";
 
 test("1 test with Sulfuras item", async () => {
   runGoldenMaster(async () => {
@@ -11,7 +100,7 @@ test("1 test with Sulfuras item", async () => {
 
 test("2 test with Aged Brie updated 1x with 10 for sellIn and quality", async () => {
   runGoldenMaster(async () => {
-    const item1 = new Item("Aged Brie", 10, 10 );
+    const item1 = new Item("Aged Brie", 10, 10);
     const items = new GildedRose([item1]);
     items.updateQuality();
   });
@@ -19,7 +108,7 @@ test("2 test with Aged Brie updated 1x with 10 for sellIn and quality", async ()
 
 test("3 test with Aged Brie when sellIn is negative", async () => {
   runGoldenMaster(async () => {
-    const item1 = new Item("Aged Brie", 2, 10 );
+    const item1 = new Item("Aged Brie", 2, 10);
     const items = new GildedRose([item1]);
     items.updateQuality();
     items.updateQuality();
@@ -29,7 +118,11 @@ test("3 test with Aged Brie when sellIn is negative", async () => {
 
 test("4 test with Backstage passes to a TAFKAL80ETC concert when", async () => {
   runGoldenMaster(async () => {
-    const item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10 );
+    const item1 = new Item(
+      "Backstage passes to a TAFKAL80ETC concert",
+      5,
+      10
+    );
     const items = new GildedRose([item1]);
     items.updateQuality();
     items.updateQuality();
@@ -38,7 +131,11 @@ test("4 test with Backstage passes to a TAFKAL80ETC concert when", async () => {
 
 test("5 test with Backstage passes to a TAFKAL80ETC concert when sellIn is negative", async () => {
   runGoldenMaster(async () => {
-    const item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 2, 10 );
+    const item1 = new Item(
+      "Backstage passes to a TAFKAL80ETC concert",
+      2,
+      10
+    );
     const items = new GildedRose([item1]);
     items.updateQuality();
     items.updateQuality();
@@ -48,8 +145,12 @@ test("5 test with Backstage passes to a TAFKAL80ETC concert when sellIn is negat
 
 test("6 test with several items when quality goes over 50", async () => {
   runGoldenMaster(async () => {
-    const item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 48 );
-    const item2 = new Item("Aged Brie", 8, 48 );
+    const item1 = new Item(
+      "Backstage passes to a TAFKAL80ETC concert",
+      15,
+      48
+    );
+    const item2 = new Item("Aged Brie", 8, 48);
     const items = new GildedRose([item1, item2]);
     items.updateQuality();
     items.updateQuality();
@@ -58,17 +159,9 @@ test("6 test with several items when quality goes over 50", async () => {
   });
 });
 
-
-
-
-
-
-
-
-
 test("7 test with conjured", async () => {
   runGoldenMaster(async () => {
-    const item1 = new Item("Conjured", 15, 3 );
+    const item1 = new Item("Conjured", 15, 3);
     const items = new GildedRose([item1]);
     items.updateQuality();
     items.updateQuality();
@@ -77,9 +170,9 @@ test("7 test with conjured", async () => {
   });
 });
 
-test("7 test with conjured", async () => {
+test("8 test with conjured (2)", async () => {
   runGoldenMaster(async () => {
-    const item1 = new Item("Conjured", 3, 45 );
+    const item1 = new Item("Conjured", 3, 45);
     const items = new GildedRose([item1]);
     items.updateQuality();
     items.updateQuality();
--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,177 @@
-type ItemsName =
+// type ItemsName =
+//   "Aged Brie"
+//   | "Backstage passes to a TAFKAL80ETC concert"
+//   | "Sulfuras, Hand of Ragnaros"
+//   | "Conjured";
+
+// export class Item {
+//   name: ItemsName;
+//   sellIn: number;
+//   quality: number;
+
+//   constructor(name, sellIn, quality) {
+//     this.name = name;
+//     this.sellIn = sellIn;
+//     this.quality = quality;
+//   }
+// }
+
+// export class GildedRose {
+//   items: Array<Item>;
+
+//   constructor(items = [] as Array<Item>) {
+//     this.items = items;
+//   }
+
+//   updateQuality() {
+//     console.log(`Mise à jour quotidienne de la qualité des items`);
+//     for (let i = 0; i < this.items.length; i++) {
+//       console.log(`Item n° ${i + 1} - nom : ${this.items[i].name} avec une qualité de ${this.items[i].quality}`);
+//       if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+//         if (this.items[i].quality > 0) {
+//           if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+//             this.items[i].quality = this.items[i].quality - 1;
+//             console.log(`La qualité diminue de 1, elle est maintenant de ${this.items[i].quality}`);
+//           }
+//         }
+//       } else {
+//         if (this.items[i].quality < 50) {
+//           this.items[i].quality = this.items[i].quality + 1;
+//           console.log(`La qualité augmente de 1, elle est maintenant de ${this.items[i].quality}`);
+//           if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+//             console.log(`SellIn est égal à ${this.items[i].sellIn}`);
+//             if (this.items[i].sellIn < 11) {
+//               if (this.items[i].quality < 50) {
+//                 this.items[i].quality = this.items[i].quality + 1;
+//                 console.log(`La qualité augmente de 1 car sellIn < 11, elle est maintenant de ${this.items[i].quality}`);
+//               }
+//             }
+//             if (this.items[i].sellIn < 6) {
+//               if (this.items[i].quality < 50) {
+//                 this.items[i].quality = this.items[i].quality + 1;
+//                 console.log(`La qualité augmente de 1 car sellIn < 6, elle est maintenant de ${this.items[i].quality}`);
+//               }
+//             }
+//           }
+//         }
+//       }
+//       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+//         this.items[i].sellIn = this.items[i].sellIn - 1;
+//         console.log(`SellIn prend -1 et est maintenant de ${this.items[i].sellIn}`);
+//       }
+//       if (this.items[i].sellIn < 0) {
+//         if (this.items[i].name != "Aged Brie") {
+//           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+//             if (this.items[i].quality > 0) {
+//               if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+//                 this.items[i].quality = this.items[i].quality - 1;
+//               }
+//             }
+//           } else {
+//             this.items[i].quality = this.items[i].quality - this.items[i].quality;
+//             console.log(`Délai de vente dépassé : la qualité de ${this.items[i].name} est maintenant de ${this.items[i].quality}`);
+//           }
+//         } else {
+//           if (this.items[i].quality < 50) {
+//             this.items[i].quality = this.items[i].quality + 1;
+//             console.log(`${this.items[i].name} prend quand même en qualité (+1), elle est maintenant de ${this.items[i].quality}`);
+//           }
+//         }
+//       }
+//     }
+//     console.log(`Fin de la mise à jour quotidienne de la qualité des items`);
+//     return this.items;
+//   }
+// }
+
+type ItemName =
   "Aged Brie"
   | "Backstage passes to a TAFKAL80ETC concert"
   | "Sulfuras, Hand of Ragnaros"
   | "Conjured";
 
 export class Item {
-  name: ItemsName;
-  sellIn: number;
-  quality: number;
-
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+  constructor(public name: ItemName, public sellIn: number, public quality: number) { }
 }
 
 export class GildedRose {
-  items: Array<Item>;
+  constructor(private items: Item[] = []) { }
 
-  constructor(items = [] as Array<Item>) {
-    this.items = items;
+  private increaseQuality(item: Item, value: number = 1) {
+    if (item.quality < 50) {
+      item.quality += value;
+    }
+  }
+
+  private decreaseQuality(item: Item, value: number = 1) {
+    if (item.quality > 0) {
+      item.quality -= value;
+    }
+  }
+
+  private updateNormalItem(item: Item) {
+    this.decreaseQuality(item);
+    item.sellIn -= 1;
+    if (item.sellIn < 0) {
+      this.decreaseQuality(item);
+    }
+  }
+
+  private updateAgedBrie(item: Item) {
+    this.increaseQuality(item);
+    item.sellIn -= 1;
+    if (item.sellIn < 0) {
+      this.increaseQuality(item);
+    }
+  }
+
+  private updateBackstagePasses(item: Item) {
+    this.increaseQuality(item);
+    if (item.sellIn < 11) {
+      this.increaseQuality(item);
+    }
+    if (item.sellIn < 6) {
+      this.increaseQuality(item);
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    }
+  }
+
+  private updateConjured(item: Item) {
+    this.decreaseQuality(item, 2);
+    item.sellIn -= 1;
+    if (item.sellIn < 0) {
+      this.decreaseQuality(item, 2);
+    }
   }
 
   updateQuality() {
     console.log(`Mise à jour quotidienne de la qualité des items`);
-    for (let i = 0; i < this.items.length; i++) {
-      console.log(`Item n° ${i + 1} - nom : ${this.items[i].name} avec une qualité de ${this.items[i].quality}`);
-      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-            console.log(`La qualité diminue de 1, elle est maintenant de ${this.items[i].quality}`);
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          console.log(`La qualité augmente de 1, elle est maintenant de ${this.items[i].quality}`);
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            console.log(`SellIn est égal à ${this.items[i].sellIn}`);
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-                console.log(`La qualité augmente de 1 car sellIn < 11, elle est maintenant de ${this.items[i].quality}`);
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-                console.log(`La qualité augmente de 1 car sellIn < 6, elle est maintenant de ${this.items[i].quality}`);
-              }
-            }
-          }
-        }
+    for (const item of this.items) {
+      console.log(`Item - nom : ${item.name} avec une qualité de ${item.quality}`);
+      switch (item.name) {
+        case "Aged Brie":
+          this.updateAgedBrie(item);
+          break;
+        case "Backstage passes to a TAFKAL80ETC concert":
+          this.updateBackstagePasses(item);
+          break;
+        case "Sulfuras, Hand of Ragnaros":
+          // Ignorer la mise à jour pour "Sulfuras"
+          break;
+        case "Conjured":
+          this.updateConjured(item);
+          break;
+        default:
+          this.updateNormalItem(item);
+          break;
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-        console.log(`SellIn prend -1 et est maintenant de ${this.items[i].sellIn}`);
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-            console.log(`Délai de vente dépassé : la qualité de ${this.items[i].name} est maintenant de ${this.items[i].quality}`);
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-            console.log(`${this.items[i].name} prend quand même en qualité (+1), elle est maintenant de ${this.items[i].quality}`);
-          }
-        }
-      }
+      console.log(`La qualité est maintenant de ${item.quality}, SellIn est égal à ${item.sellIn}`);
     }
     console.log(`Fin de la mise à jour quotidienne de la qualité des items`);
     return this.items;
   }
 }
+
