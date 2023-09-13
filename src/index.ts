1274b2d0354c36dcc733d828fdbfbf93d261ef6e
Amaury Franssen <amauryfranssen@gmail.com>
2023-09-13 09:07:31 +0200

    feat: add gilded rose class with new pattern

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,18 @@
-import { Item, Items } from "./Item";
-import { changeQuality, UpdateItemQuality } from "./QualityStatus";
-import { changeSellIn } from "./SellInDelay";
+import { Item } from "./Item";
 
-export class GildedRose extends Items {
+export class GildedRose {
+  items: Array<Item>;
 
-  constructor(items = [] as Array<Item>) {
-    super(items);
+  constructor(items :Array<Item>) {
+    this.items = items;
   }
-
-  changeSellIn = new changeSellIn(this.items);
-  changeQuality = new changeQuality(this.items);
-  MAX_QUALITY = this.changeQuality.getMaximumQuality();
-
-  UpdateItemQuality = new UpdateItemQuality(this.items);
 
   updateQuality() {
     console.log(`Mise à jour quotidienne de la qualité des items`);
+
     this.items.forEach((item, index) => {
-
-      console.log(`Item n° ${index + 1} - nom : ${this.items[index].name} avec une qualité de ${this.items[index].quality}`);
-
-      switch (this.items[index].name) {
-        case "Aged Brie":
-          this.UpdateItemQuality.updateQualityAgedBrie(index);
-          this.changeSellIn.decreaseSellIn(index);
-          if (this.items[index].sellIn < 0) {
-            console.log(`Délai de vente dépassé`);
-            console.log(`La qualité de ${this.items[index].name} augmente quand même`);
-            this.UpdateItemQuality.updateQualityAgedBrie(index);
-          }
-          break;
-        case "Backstage passes to a TAFKAL80ETC concert":
-          this.UpdateItemQuality.updateQualityBackstagePasses(index);
-          this.changeSellIn.decreaseSellIn(index);
-          if (this.items[index].sellIn < 0) {
-            console.log(`Délai de vente dépassé`);
-            this.changeQuality.setQualityToZero(index);
-          }
-          break;
-        case "Conjured":
-          this.UpdateItemQuality.updateQualityConjured(index);
-          this.changeSellIn.decreaseSellIn(index);
-
-      }
-
+      console.log(`Item n° ${index + 1} - nom : ${item.name} avec une qualité de ${item.quality}`);
+      item.update();
     });
     console.log(`Fin de la mise à jour quotidienne de la qualité des items`);
     return this.items;
